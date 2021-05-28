a6c69ff8843bc22713b434675b5932765bc5221b
Alexander Bibikov <abibikov.me@gmail.com>
2021-05-28 20:19:09 +0300

    form submit log added

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
     const phoneMask = () => {
         let phoneFields = document.querySelectorAll('input[data-input=phone]');
+        let form = document.querySelector('form');
+        console.log(form)
 
-        const inputValueToDigits = function (input) {
+        const inputValueToDigits = (input) => {
             return input.value.replace(/\D/g, "");
         }
 
@@ -47,7 +49,7 @@ document.addEventListener('DOMContentLoaded', () => {
             input.value = formattedInput;
         }
 
-        const onPaste = function (e) {
+        const onPaste = (e) => {
             let input = e.target,
                 inputNumbersValue = inputValueToDigits(input);
             let pasted = e.clipboardData || window.clipboardData;
@@ -60,11 +62,31 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         }
 
-        const onKeyDown = function (e) {
+        const onKeyDown = (e) => {
             let inputValue = e.target.value.replace(/\D/g, '');
             if (e.keyCode === 8 && inputValue.length === 1) {
                 e.target.value = "";
             }
+        }
+
+        const onSubmit = (e) => {
+            e.preventDefault();
+            const input = e.currentTarget[0].value;
+            let phoneFormatted = input.replace(/\D/g, "");
+            if (phoneFormatted[0].indexOf("8") > -1) {
+                phoneFormatted = phoneFormatted.replace("8", "7");
+            }
+
+            const inputData = {
+                phone: phoneFormatted,
+                length: phoneFormatted.length
+            }
+            console.log("%c Phone number normalized for backend:", "color: #549CFD", phoneFormatted);
+            console.log("%c Number of digits:", "color: #549CFD", phoneFormatted.length);
+            console.log("%c You can receive data as normalized string or as object with custom fields.", "color: #005FE0");
+
+            alert(`Form submitted, please check console for details. Phone number normalized for backend: ${JSON.stringify(inputData)}`);
+            return inputData;
         }
 
         for (let phoneInput in phoneFields) {
@@ -74,6 +96,8 @@ document.addEventListener('DOMContentLoaded', () => {
             input.addEventListener('input', onPaste, false);
             input.addEventListener('input', onKeyDown, false);
         }
+
+        form.addEventListener('submit', onSubmit);
     };
 
     phoneMask();
